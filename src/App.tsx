8c99d05062358c6a7f2594af98d9952859625dd0
47URSAY <116015494+47URSAY@users.noreply.github.com>
2025-05-20 14:47:14 +0200

    Added .env

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Posts from './pages/Posts';
+import News from './pages/News';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import CreatePost from './pages/CreatePost';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
+        <main className="container mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/posts" element={<Posts />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/create-post" element={<CreatePost />} />
+          </Routes>
+        </main>
+        <Toaster position="bottom-right" />
+      </div>
+    </Router>
+  );
+}
